3f774cc0925249bc06545f582a50f9c4dea34810
AlbeTroll <albertocastilloAC05@gmail.com>
2024-02-03 00:46:04 +0100

    tyyweye

--- mail.py
+++ mail.py
@@ -7,5 +7,5 @@ def sendemail(message):
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
     server.login(email, password)
-    server.sendmail(email, send_to_email, message)
+    server.sendmail(email, send_to_email, message.decode("utf-8"))
     server.quit()
--- rastreador_precios_casero.py
+++ rastreador_precios_casero.py
@@ -6,6 +6,8 @@ import time
 URL = "https://www.amazon.es/Hercules-DJControl-Inpulse-300-MK2/dp/B0B9LBVDVV/ref=sr_1_1?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=1UEDN356HR344&keywords=dj+control+inpulse+300+mk2&qid=1706830231&sprefix=dj+control+inpulse+00+mk2%2Caps%2C118&sr=8-1&ufe=app_do%3Aamzn1.fos.5e544547-1f8e-4072-8c08-ed563e39fc7d"
 HEADER = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"}
 
+str(u'\u2013')
+
 def getprice():
     page = requests.get(URL, headers = HEADER)
     soup = BeautifulSoup(page.content, "lxml")
@@ -24,7 +26,7 @@ def readFile():
 def writeFile(number):
     f = open("price.txt", "w")
     if f.mode == "w":
-        f.write(number)
+        f.write(str(number))
     f.close()
     
 def main():
@@ -37,16 +39,20 @@ def main():
     while(True):
         oldprice = readFile()
         newprice = getprice()
+        print(oldprice)
+        print(newprice)
         if i == 2:
             newprice = 50
         if float(newprice) < float(oldprice):
             message = f"Su producto ha sido rebajado a {newprice} euros, cómpralo cabronazo \n URL: {URL}"
+            message = message.encode('utf-8')
+
             mail.sendemail(message)
             writeFile(newprice)
             print("Mensaje mandado \n")
         
         i += 1
-        time.sleep(5)
+        time.sleep(2)
 
 if __name__ == "__main__":
     main()
